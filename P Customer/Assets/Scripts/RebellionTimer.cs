17a265fefd9d80b72115f0a0563481eaf03d5d34
Gristicle <theodoreg23456@gmail.com>
2023-09-06 14:30:20 +0200

    Rebellion timer added

---
+++ P Customer/Assets/Scripts/RebellionTimer.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RebellionTimer : MonoBehaviour
+{
+    public float currentRebellion;
+    public int maxRebellion;
+    public bool gameOver;
+    public float rebellionModifier;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameOver = false;
+        maxRebellion = 400;
+        currentRebellion = 0;
+        rebellionModifier = 5f;
+    }
+
+    private void FixedUpdate()
+    {
+        currentRebellion += (Time.deltaTime * rebellionModifier);
+        Debug.Log(Mathf.Round(currentRebellion/3));
+        if(currentRebellion >= maxRebellion)
+        {
+            gameOver = true;
+        }
+        if (gameOver)
+        {
+            Debug.Log("Time's up, dumbass");
+        }
+    }
+}
