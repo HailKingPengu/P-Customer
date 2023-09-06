8106762f4867ea11a89b20d7b87a23eed2a360a4
minimanx00 <msengers98@gmail.com>
2023-09-06 16:16:42 +0200

    random movement for weird guy (broken)

---
+++ P Customer/Assets/Scripts/MoveRandom.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRandom : MonoBehaviour
+{
+
+    Rigidbody rb;
+    Vector3 dir;
+    public float force;
+    int timer = 30;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        dir = ChooseRandom();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(dir * force);
+    }
+
+    private void FixedUpdate()
+    {
+        timer--;
+        if (timer <= 0)
+        {
+            dir = ChooseRandom();
+            timer = 30;
+        }
+    }
+
+    Vector3 ChooseRandom ()
+    {
+        float r1 = Random.Range(0f, 1f);
+        float r2 = Random.Range(0f, 1f);
+        return (new Vector3(r1, 0f, r1));
+    }
+}
