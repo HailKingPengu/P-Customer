40304e01a34396e9a6b6a9dfba13832a7172f950
minimanx00 <msengers98@gmail.com>
2023-09-19 12:50:51 +0200

    Big protests, Increasing Protests

--- P Customer/Assets/RebelSpawner.cs
+++ P Customer/Assets/RebelSpawner.cs
@@ -2,28 +2,123 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using TMPro;
 
 public class RebelSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject rebelObject;
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] private List<string> defaultCritic;
+    [SerializeField] private List<string> happinessCritic;
+    [SerializeField] private List<string> powerCritic;
+
+    [SerializeField] private TMPro.TextMeshProUGUI textmesh;
+
+    [SerializeField] private AudioSource protestSounds;
+    private enum thingsToBeMadAbout { 
+        pollution,
+        happiness,
+        power
+    }
+    [SerializeField] private thingsToBeMadAbout theThingIAmMadAbout;
+
+    GameManager manager;
+
+    float timer;
+    [SerializeField] private float timeTillUpdate;
+
+    public string madAboutString;
+    float percentageOfProtestors = 0;
+
+    float rebelsCreated = 0;
+
+   // Start is called before the first frame update
+   void Start()
     {
-        for (int i = 0;i < 10;i++) {
-            CreateRebel();
-        }
+        manager = GameManager.Instance;
+        CreateRebel();
+        UpdateMadChoice();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        if (timer > timeTillUpdate)
+        {
+            timer -= timeTillUpdate;
+            UpdateMadChoice();
+        }
+    }
+
+    void UpdateMadChoice()
+    {
+        if (manager.powerUse > manager.powerProduction)
+        {
+
+            theThingIAmMadAbout = thingsToBeMadAbout.power;
+
+        }
+        else
+        if (manager.happiness < 50)
+        {
+
+            theThingIAmMadAbout = thingsToBeMadAbout.happiness;
+
+        }
+        else
+        {
+
+            theThingIAmMadAbout = thingsToBeMadAbout.pollution;
+
+        }
+
+
+        switch (theThingIAmMadAbout)
+        {
+            case thingsToBeMadAbout.power:
+                chooseRandomString(powerCritic);
+                break;
+            case thingsToBeMadAbout.happiness:
+                chooseRandomString(happinessCritic);
+                break;
+            case thingsToBeMadAbout.pollution:
+                chooseRandomString(defaultCritic);
+                break;
+        }
+
+        RebellionTimer timer = manager.GetComponent<RebellionTimer>();
+
+        float newProtestorPercent = (timer.currentRebellion / timer.maxRebellion);
+
+        if(newProtestorPercent*10f > rebelsCreated)
+        {
+            CreateRebel();
+        }
+
+
+        protestSounds.volume = newProtestorPercent * 0.8f + 0.2f;
+    }
+
+    void chooseRandomString(List<string> stringList)
+    {
+        int index = Mathf.RoundToInt(Random.Range(0,stringList.Count));
+        madAboutString = stringList[index];
+        textmesh.text = madAboutString;
     }
 
     public void CreateRebel()
     {
-        Debug.Log("HELP");
          GameObject newObject = Instantiate(rebelObject, transform.position + new Vector3(Random.Range(-1f, 1f), 0, Random.Range(-1f, 1f)), transform.rotation);
+         rebelsCreated++;
+    }
 
+     void OnMouseEnter()
+    {
+        textmesh.gameObject.SetActive(true);
+    }
+     void OnMouseExit()
+    {
+        textmesh.gameObject.SetActive(false);
     }
 }
